4ae5e1c88c58c6d0d21916d4e9d5ef9fdd732531
jeffyactive <jeff@reelyactive.com>
2016-02-23 12:42:00 -0500

    Updated to real-time distribution and events

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,9 +1,9 @@
 DEFAULT_SOCKET_URL = 'http://www.hyperlocalcontext.com/notman';
 DEFAULT_ASSOCIATIONS_URL = 'http://www.hyperlocalcontext.com/associations/';
-SEGMENTS = ['appearances', 'keepalives', 'disappearances', 'displacements'];
+EVENT_HISTORY = 10;
 
 angular.module('dashboard', [ 'btford.socket-io', 'reelyactive.beaver',
-                              'reelyactive.cormorant' ])
+                              'reelyactive.cormorant', 'ngSanitize' ])
 
   // Socket.io factory
   .factory('Socket', function(socketFactory) {
@@ -12,48 +12,90 @@ angular.module('dashboard', [ 'btford.socket-io', 'reelyactive.beaver',
 
 
   // Dashboard controller
-  .controller('DashCtrl', function($scope, $interval, $http, Socket, beaver,
-                                   cormorant) {
+  .controller('DashCtrl', function($scope, $interval, $http, $sce, Socket,
+                                   beaver, cormorant) {
 
+    var chart;
     $scope.devices = beaver.getDevices();
     $scope.stats = beaver.getStats();
+    $scope.events = [];
+    $scope.chartLegend = '<h1>Waiting for initial event...</h1>';
+
     beaver.listen(Socket);
 
 
-    var chart;
+    // Action triggers: appearance, displacement and disappearance
+    Socket.on('appearance', function(data) {
+      updateChart();
+      updateEvents('appearance', data);
+    });
+    Socket.on('displacement', function(data) {
+      updateChart();
+      updateEvents('displacement', data);
+    });
+    Socket.on('disappearance', function(data) {
+      updateChart();
+      updateEvents('disappearance', data);
+    });
 
     function initChart() {
       var ctx = document.getElementById('chart').getContext('2d');
       chart = new Chart(ctx).Doughnut([
         {
           value: 0,
-          color: "#F7464A",
-          highlight: "#FF5A5E",
-          label: "Appearances"
+          color: "#ff6900",
+          highlight: "#f8b586",
+          label: "Main Floor"
         }, {
           value: 1,
-          color: "#46BFBD",
-          highlight: "#5AD3D1",
-          label: "Keep-alives"
+          color: "#0770a2",
+          highlight: "#82b6cf",
+          label: "Second Floor"
         }, {
           value: 0,
-          color: "#FDB45C",
-          highlight: "#FFC870",
-          label: "Disappearances"
-        }, {
-          value: 0,
-          color: "#FDB45C",
-          highlight: "#FFC870",
-          label: "Displacements"
+          color: "#aec844",
+          highlight: "#d0dd9e",
+          label: "Third Floor"
       }]);
+      $scope.chartLegend = $sce.trustAsHtml(chart.generateLegend());
     }
 
-    $scope.$watch('stats', function(stats) {
-      if (chart == null) { initChart(); return };
-      for (var i = 0; i < SEGMENTS.length; i++) {
-        chart.segments[i].value = stats[SEGMENTS[i]];
+    function updateChart() {
+      if(chart == null) {
+        initChart();
+        return;
+      }
+      chart.segments[0].value = 0;
+      chart.segments[1].value = 0;
+      chart.segments[2].value = 0;
+      for(deviceId in $scope.devices) {
+        var device = $scope.devices[deviceId];
+        var strongestDecoder = device.tiraid.radioDecodings[0]
+                              .identifier.value;
+        switch(strongestDecoder) {
+          case '001bc509408100d9':
+          case '001bc509408100da':
+          case '001bc509408100db':
+            chart.segments[0].value++; // Main floor
+            break;
+          case '001bc509408100dc':
+          case '001bc509408100dd':
+          case '001bc509408100de':
+            chart.segments[1].value++; // Second floor
+            break;
+          case '001bc509408100df':
+            chart.segments[2].value++; // Third floor
+            break;
+        }
       }
       setTimeout(function() { chart.update() });
-    }, true );
+    }
+
+    function updateEvents(type, data) {
+      var length = $scope.events.unshift({ type: type, tiraid: data.tiraid });
+      if(length > EVENT_HISTORY) {
+        $scope.events.pop();
+      }
+    }
 
   });
