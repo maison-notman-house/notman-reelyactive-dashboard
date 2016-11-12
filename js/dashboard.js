c7e2825dfc53fe7ba7b0a6d0031dd9c0a7992315
jeffyactive <jeff@reelyactive.com>
2016-11-12 07:49:28 -0500

    Major revamp for HackTheHouse 3

--- js/dashboard.js
+++ js/dashboard.js
@@ -6,31 +6,17 @@
 
 // Constant definitions
 DEFAULT_SOCKET_URL = 'https://www.hyperlocalcontext.com/notman';
-MAX_RSSI = 255;
-DOUGHNUT_OPTIONS = {
-  legend: {
-    display: true,
-    labels: {
-      fontSize: 24,
-      fontColor: "#fff"
-    }
-  }
-};
 
 
 /**
  * dashboard Module
  * All of the JavaScript specific to the dashboard is contained inside this
  * angular module.  The only external dependencies are:
- * - beaver, cormorant and cuttlefish (reelyActive)
+ * - beaver, cormorant (reelyActive)
  * - socket.io (btford)
- * - ngSanitize (angular)
- * - chart.js (jtblin)
  */
 angular.module('dashboard', ['btford.socket-io', 'reelyactive.beaver',
-                             'reelyactive.cormorant',
-                             'reelyactive.cuttlefish', 'ngSanitize',
-                             'chart.js'])
+                             'reelyactive.cormorant'])
 
 
 /**
@@ -55,13 +41,8 @@ angular.module('dashboard', ['btford.socket-io', 'reelyactive.beaver',
   $scope.directories = beaver.getDirectories();
   $scope.stats = beaver.getStats();
   $scope.stories = cormorant.getStories();
-  $scope.featuredDirectory = null;
-  $scope.addFiller = true;
-  $scope.doughnutLabels = [ '3', '2', '1', 'Cafe' ];
-  $scope.doughnutData = [ 0, 0, 0, 0 ];
-  $scope.doughnutColors = [ '#83b7d1', '#0770a2', '#043851', '#ff6900' ];
-  $scope.doughnutOptions = DOUGHNUT_OPTIONS;
   $scope.numberOfDevices = 0;
+  $scope.clock = '';
 
   // beaver.js listens on the websocket for events
   beaver.listen(Socket);
@@ -69,25 +50,21 @@ angular.module('dashboard', ['btford.socket-io', 'reelyactive.beaver',
   // Handle events pre-processed by beaver.js
   beaver.on('appearance', function(event) {
     handleEvent(event);
-    updateDoughnut(event);
-    beaver.addDeviceProperty(event.deviceId, 'featured', false);
   });
   beaver.on('displacement', function(event) {
     handleEvent(event);
-    updateDoughnut(event);
   });
   beaver.on('keep-alive', function(event) {
     handleEvent(event);
   });
   beaver.on('disappearance', function(event) {
     handleEvent(event);
-    updateDoughnut(event);
   });
 
   // Handle an event
   function handleEvent(event) {
-    updateDeviceStory(event.deviceId, event.deviceUrl);
-    updateDirectoryStory(event.receiverDirectory, event.receiverUrl);
+    //updateDeviceStory(event.deviceId, event.deviceUrl);
+    //updateDirectoryStory(event.receiverDirectory, event.receiverUrl);
     $scope.numberOfDevices = Object.keys($scope.devices).length;
   }
 
@@ -108,20 +85,6 @@ angular.module('dashboard', ['btford.socket-io', 'reelyactive.beaver',
                                     story['@graph'][0]['schema:image']);
       }
     });
-  }
-
-  // Update the doughnut chart
-  function updateDoughnut(event) {
-    $scope.doughnutData[0] = getNumberOfDirectoryDevices('notman:third:west')
-                         + getNumberOfDirectoryDevices('notman:third:centre')
-                         + getNumberOfDirectoryDevices('notman:third:east');
-    $scope.doughnutData[1] = getNumberOfDirectoryDevices('notman:second:west')
-                         + getNumberOfDirectoryDevices('notman:second:centre')
-                         + getNumberOfDirectoryDevices('notman:second:east');
-    $scope.doughnutData[2] = getNumberOfDirectoryDevices('notman:first:west')
-                         + getNumberOfDirectoryDevices('notman:first:centre')
-                         + getNumberOfDirectoryDevices('notman:first:east');
-    $scope.doughnutData[3] = getNumberOfDirectoryDevices('notman:cafe');
   }
 
   // Get the number of devices in the given directory
@@ -145,56 +108,11 @@ angular.module('dashboard', ['btford.socket-io', 'reelyactive.beaver',
     return false;
   }
 
-  // Update the featured directory and story
-  function updateFeatured() {
-    var people = 0;
-    var products = 0;
-    var newFeaturedDirectory = $scope.featuredDirectory;
-    for(cDirectory in $scope.directories) {
-      var currentPeople = 0;
-      var currentProducts = 0;
-      var currentDirectory = $scope.directories[cDirectory];
-      for(cDevice in currentDirectory.devices) {
-        if(currentDirectory.devices[cDevice].person) {
-          currentPeople++;
-        }
-        currentProducts++;
-      }
-      if(((currentPeople > people) ||
-          ((people === 0) && (currentProducts > products))) &&
-         (currentDirectory !== $scope.featuredDirectory)) {
-        newFeaturedDirectory = currentDirectory;
-        people = currentPeople;
-        products = currentProducts;
-      }
-    }
-    if(newFeaturedDirectory !== $scope.featuredDirectory) {
-      $scope.featuredDirectory = newFeaturedDirectory;
-    }
-    else {
-      var directories = Object.keys($scope.directories);
-      var randomIndex = Math.floor(Math.random() * directories.length);
-      $scope.featuredDirectory = $scope.directories[directories[randomIndex]];
-    }
-
-    var featuredStories = [];
-    for(cDevice in $scope.devices) {
-      beaver.addDeviceProperty(cDevice, 'featured', false);
-      if($scope.devices[cDevice].person === true) {
-        featuredStories.push(cDevice);
-      }
-    }
-    if(featuredStories.length > 0) {
-      $scope.addFiller = false;
-      var featuredStoryIndex = Math.floor(Math.random() *
-                                          featuredStories.length);
-      beaver.addDeviceProperty(featuredStories[featuredStoryIndex],
-                               'featured', true);
-    }
-    else {
-      $scope.addFiller = true;
-    }
+  // Update the time every second
+  var tick = function() {
+    var now = new Date();
+    $scope.clock = now.getHours() + 'h' + now.getMinutes();
   }
+  setInterval(tick, 1000);
 
-  setInterval(updateFeatured, 12000);
 });
